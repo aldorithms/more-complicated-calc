5752faeb5c76224a5546cbaf695e45e7319c6344
Aldorithms <99375833+aldorithms@users.noreply.github.com>
2023-03-26 22:25:34 -0700

    init

---
+++ Calculator.h
@@ -0,0 +1,21 @@
+class Calculator
+{
+    private:
+    double n1, n2;
+    char key, op;
+
+    public:
+    Calculator();
+    ~Calculator();
+};
+
+Calculator::Calculator()
+{
+    
+    return this;
+}
+
+Calculator::~Calculator()
+{
+
+}
